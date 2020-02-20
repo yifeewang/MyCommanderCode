2523b0b3651e1eebbde44b22feb60578751dd491
yifeewang <896678534@qq.com>
2020-02-20 10:29:35 +0800

    feat(开始工程化): commit

--- bin/download.js
+++ bin/download.js
@@ -1,24 +1,35 @@
-const fs = require('fs')
-const path = require('path');
-const chalk = require('chalk');
-const commander = require('commander');
+let fs = require('fs')
+let path = require('path');
+let chalk = require('chalk');
+let commander = require('commander');
 
 module.exports = answer => {
-    const demoPath = '../index.js';
-    const targetPath = '../' + answer;
-    const ob = [];
+    let demoPath = '/project';
+    let targetPath = '/' + answer;
+    let ob = [];
 
-    const download = () => {
-        getPath(demoPath)
-        console.log(ob)
+    let download = () => {
+        fs.mkdir(process.cwd() + targetPath, async function(){
+            await getPath(demoPath)
+            console.log(ob)
+            ob.forEach((item, index) => {
+                if(item[0] ==='file') {
+                    fs.readFile(process.cwd() + item[1], function(err, data){
+                        fs.writeFile(process.cwd() + targetPath+ item[1].replace("/project","/"), data, function(err){
+                            console.log(chalk.blue(`${process.cwd() + targetPath+ item[1].replace("/project","/")}`)+ chalk.yellow(' has been dowmload success'))
+                        } )
+                    })
+                }else{
+                    fs.mkdir(process.cwd() + targetPath + item[1].replace("/project",""),function () {})
+                }
+            })
+        })
     }
-    const getPath = (path) => {
-        const files = fs.readdirSync(path);
-        console.log(111,files);
-        
+    let getPath = (path) => {
+        let files = fs.readdirSync(process.cwd() + path);
         files.forEach(item => {
             let nowPath = path + '/' + item;
-            let statNews = fs.statSync(nowPath);
+            let statNews = fs.statSync(process.cwd() + nowPath);
             if(statNews.isDirectory()) {
                 ob.push(['dir', nowPath])
                 getPath(nowPath);
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const ora = require('ora');
-// const fs = require('fs');
-// const inquirer = require('inquirer');
-// const commander = require('commander');
-// const chalk = require('chalk');
-// const download = require('./bin/download.js');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const commander = require('commander');
+const chalk = require('chalk');
+const download = require('./bin/download.js');
 
-// const spinner = ora(chalk.yellow('loading start,please wait a little!')).start();
+commander
+  .version('0.1.0', '-v, --version')
+  .option('-p, --peppers', 'Add peppers')
+  .parse(process.argv);
 
-download('newDir')
+const spinner = ora(chalk.yellow('loading start,please wait a little!')).start();
+
+const startDownload = async() => {
+    await download('newDir')
+    setTimeout(() => {
+        spinner.stop()
+    }, 4000);
+}
+startDownload()
